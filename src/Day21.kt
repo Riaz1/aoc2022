a387a3ec937fa6b3cded876299f02cc4ae65e539
Riaz <mriazs@gmail.com>
2022-12-22 09:47:40 +0200

    Day 21 tested and works

---
+++ src/Day21.kt
@@ -0,0 +1,64 @@
+data class LineItem(
+    val key1: String,
+    val operator: String,
+    val key2: String,
+    var result: Double?
+)
+
+fun main() {
+    val fileName =
+        "Day21_input"
+//        "Day21_sample"
+
+    val steps = readInput(fileName).associateBy(
+        keySelector = {v -> v.substringBefore(":").trim()},
+        valueTransform = {v ->
+            if (v.substringAfter(":").trim().toDoubleOrNull() != null) {
+                LineItem("", "", "", v.substringAfter(":").trim().toDoubleOrNull())
+            } else {
+                LineItem(
+                    v.substringAfter(":").trim().split(" ")[0],
+                    v.substringAfter(":").trim().split(" ")[1],
+                    v.substringAfter(":").trim().split(" ")[2],
+                    null
+                )
+            }
+        })
+
+    var rootValue : Double? = null
+    while (rootValue == null) {
+        run breaking@ {
+            steps.forEach { (key, item) ->
+                if (key == "root" && item.result != null) {
+                    rootValue = item.result
+                    return@breaking
+                }
+
+                if (item.result == null &&
+                    steps[item.key1]?.result != null &&
+                    steps[item.key2]?.result != null) {
+                    steps[key]?.result = when (item.operator) {
+                        "+" -> steps[item.key2]?.result?.let {
+                            steps[item.key1]?.result?.plus(it)
+                        }
+                        "-" -> {
+                            steps[item.key2]?.result?.let {
+                                steps[item.key1]?.result?.minus(it)
+                            }
+                        }
+                        "*" -> steps[item.key2]?.result?.let {
+                            steps[item.key1]?.result?.times(it)
+                        }
+                        "/" -> steps[item.key2]?.result?.let {
+                            steps[item.key1]?.result?.div(it)
+                        }
+                        else -> null
+                    }
+                }
+            }
+        }
+    }
+
+    rootValue.println()
+    "%.2f".format(rootValue).println()
+}
